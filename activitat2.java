773bb492c7cd69ce0e595a560b92b2636986ee74
richard02004 <richardroiget@iesmontsia.org>
2023-01-31 10:36:15 +0100

    pujar_arxius

---
+++ activitat2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class activitat2 {
+    public static void main (String[]arg) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        boolean c = ( a <= b);
+        System.out.println(c);
+
+    }
+}
+
